e474dfa328cda46a8adc678adc16e3b46fe370a4
bakyunsu00 <zgustn0@gmail.com>
2025-12-08 01:48:34 +0900

    feat: 상품 등록 처리 추가

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -8,8 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -33,6 +36,44 @@ public class BasicItemController {
         return "/basic/item";
     }
 
+
+    @GetMapping("/add")
+    public String addFrom(){
+        return "/basic/addForm";
+    }
+
+
+
+//    @PostMapping("/add")
+//    public String save(@RequestParam String itemName, Integer price, Integer quantity, Model model){
+//        Item item =  new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//        itemRepository.save(item);
+//        model.addAttribute("item",item);
+//        return "/basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String save(@ModelAttribute("item") Item item, Model model){
+//        itemRepository.save(item);
+//        model.addAttribute("item",item);
+//        return "/basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String save(@ModelAttribute Item item){
+//        itemRepository.save(item);
+//        return "/basic/item";
+//    }
+
+
+    @PostMapping("/add")
+    public String save(Item item){
+        itemRepository.save(item);
+        return "/basic/item";
+    }
 
 
     @PostConstruct
