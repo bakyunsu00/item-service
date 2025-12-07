93b79633edfe1c8452f5ed549752b602bba745e9
bakyunsu00 <zgustn0@gmail.com>
2025-12-07 20:22:38 +0900

    feat: 상품목록, 상세 추가

--- src/main/java/hello/itemservice/domain/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/ItemRepository.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ItemRepository {
 
     private static Long sequence = 0L;
